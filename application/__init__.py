040d6648a6a4a840536f74a232ff0f5860fb90ac
Daniel Cabral <danielccabral1981@gmail.com>
2021-04-13 20:32:57 -0300

    Alterando Videos para otimização

--- application/__init__.py
+++ application/__init__.py
@@ -13,8 +13,8 @@ listvideo = []
 listvideo.append(Video("/videos/vingadores.mp4", "/img/vingadores.png", "Vingadores Ultimato", "Ação", 0, 0, "25/04/2019"))
 listvideo.append(Video("/videos/ultimomen.mp4", "/img/ultimomen.png", "Ultimo Homem", "Realidade", 0, 0, "26/01/2017"))
 listvideo.append(Video("/videos/peakyblienders1.mp4", "/img/pky.jpg", "Peaky Blinders", "by order of the peaky blinders", 0, 0, "12/09/2013"))
-listvideo.append(Video("/videos/breakingbad1.mp4", "/img/break.png", "Breaking Bad", "Drogas", 0, 0, "17/07/2008"))
-listvideo.append(Video("/videos/onepiece.mp4", "/img/onepiecezada.png", "One Piece", "Piratas", 0, 0, "22/07/1997"))
+listvideo.append(Video("/videos/break1.mp4", "/img/break.png", "Breaking Bad", "Drogas", 0, 0, "17/07/2008"))
+listvideo.append(Video("/videos/onepiece1.mp4", "/img/onepiecezada.png", "One Piece", "Piratas", 0, 0, "22/07/1997"))
 listvideo.append(Video("/videos/tatakae.mp4", "/img/tatakae.png", "Atack ao titan", "Titans", 0, 0, "10/11/2010"))
 
 categorylist = []
